1d1c60b7bccf543f68f5e40d12d109e59a2a0c36
CedArctic <kdoumani98@gmail.com>
2020-01-01 13:17:12 +0200

    Added V1 Implementation
    CUDA with 2D grid:
    -Single moment per thread
    -11x11 sized blocks (=121 threads per block)
    -47x47 sized grid (=2209 blocks)

---
+++ cuda_single_moment_threads.c
@@ -0,0 +1,173 @@
+// Block size axis (BLOCK_SIZE^2 = number of threads per block)
+#define BLOCK_SIZE 11
+// Number of blocks on axis (GRID_SIZE^2 = number of blocks in grid)
+#define GRID_SIZE 47
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//! Ising model evolution
+/*!
+
+  \param G      Spins on the square lattice             [n-by-n]
+  \param w      Weight matrix                           [5-by-5]
+  \param k      Number of iterations                    [scalar]
+  \param n      Number of lattice points per dim        [scalar]
+
+  NOTE: Both matrices G and w are stored in row-major format.
+*/
+
+// Cuda kernel function used to calculate one moment per thread
+__global__ void cudaKernel(int n, double* gpu_w, int* gpu_G, int* gpu_gTemp){
+
+	// Calculate thread id
+	int thread_id = blockIdx.x * BLOCK_SIZE + threadIdx.x;
+
+	// Moment's coordinates (j->Y, p->X axis)
+	int p = thread_id % n;
+	int j = thread_id / n;
+
+	// Check if thread id is within bounds and execute
+	if(thread_id < n*n){
+
+		// Sum variable to decide what value a moment will take
+		int weightSum = 0;
+
+		// Unrolled weights calculations for this moment
+		weightSum += gpu_w[0] * gpu_G[((-2 + j + n) % n) * n + (-2 + p + n) % n];
+		weightSum += gpu_w[1] * gpu_G[((-2 + j + n) % n) * n + (-1 + p + n) % n];
+		weightSum += gpu_w[2] * gpu_G[((-2 + j + n) % n) * n + (p + n) % n];
+		weightSum += gpu_w[3] * gpu_G[((-2 + j + n) % n) * n + (1 + p + n) % n];
+		weightSum += gpu_w[4] * gpu_G[((-2 + j + n) % n) * n + (2 + p + n) % n];
+		weightSum += gpu_w[5] * gpu_G[((-1 + j + n) % n) * n + (-2 + p + n) % n];
+		weightSum += gpu_w[6] * gpu_G[((-1 + j + n) % n) * n + (-1 + p + n) % n];
+		weightSum += gpu_w[7] * gpu_G[((-1 + j + n) % n) * n + (p + n) % n];
+		weightSum += gpu_w[8] * gpu_G[((-1 + j + n) % n) * n + (1 + p + n) % n];
+		weightSum += gpu_w[9] * gpu_G[((-1 + j + n) % n) * n + (2 + p + n) % n];
+		weightSum += gpu_w[10] * gpu_G[((j + n) % n) * n + (-2 + p + n) % n];
+		weightSum += gpu_w[11] * gpu_G[((j + n) % n) * n + (-1 + p + n) % n];
+		weightSum += gpu_w[13] * gpu_G[((j + n) % n) * n + (1 + p + n) % n];
+		weightSum += gpu_w[14] * gpu_G[((j + n) % n) * n + (2 + p + n) % n];
+		weightSum += gpu_w[15] * gpu_G[((1 + j + n) % n) * n + (-2 + p + n) % n];
+		weightSum += gpu_w[16] * gpu_G[((1 + j + n) % n) * n + (-1 + p + n) % n];
+		weightSum += gpu_w[17] * gpu_G[((1 + j + n) % n) * n + (p + n) % n];
+		weightSum += gpu_w[18] * gpu_G[((1 + j + n) % n) * n + (1 + p + n) % n];
+		weightSum += gpu_w[19] * gpu_G[((1 + j + n) % n) * n + (2 + p + n) % n];
+		weightSum += gpu_w[20] * gpu_G[((2 + j + n) % n) * n + (-2 + p + n) % n];
+		weightSum += gpu_w[21] * gpu_G[((2 + j + n) % n) * n + (-1 + p + n) % n];
+		weightSum += gpu_w[22] * gpu_G[((2 + j + n) % n) * n + (p + n) % n];
+		weightSum += gpu_w[23] * gpu_G[((2 + j + n) % n) * n + (1 + p + n) % n];
+		weightSum += gpu_w[24] * gpu_G[((2 + j + n) % n) * n + (2 + p + n) % n];
+
+		// Decide on what future moment should be based on temp:
+		// If positive, set to 1. If negative, to -1. If 0, leave untouched
+		if(weightSum > 0.0001)
+			gpu_gTemp[j * n + p] = 1;
+		else if(weightSum < -0.0001)
+			gpu_gTemp[j * n + p] = -1;
+		else
+			gpu_gTemp[j * n + p] = gpu_G[j * n + p];
+	}
+}
+
+void printResult(int *G, int n){
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            printf("%d ", G[i*n+j]);
+        }
+        printf("\n");
+    }
+
+}
+
+void ising( int *G, double *w, int k, int n){
+
+	// Calculate number of blocks
+	//int blocks = ((n*n) % BLOCK_SIZE == 0) ? ((n*n) / BLOCK_SIZE) : ((n*n) / BLOCK_SIZE) + 1;
+
+	// Use cuda memcpy to copy weights array w to gpu memory gpu_w
+	double *gpu_w; 
+	cudaMalloc(&gpu_w, 25*sizeof(double));
+	cudaMemcpy(gpu_w, w, 25*sizeof(double), cudaMemcpyHostToDevice);
+	
+	// Array to hold the data of G in GPU memory
+	int *gpu_G;
+	cudaMalloc(&gpu_G, n*n*sizeof(int));
+	cudaMemcpy(gpu_G, G, n*n*sizeof(int), cudaMemcpyHostToDevice);
+
+	// Temporary GPU memory array used as the modified one in tandem with gpu_G
+	int *gpu_gTemp;
+	cudaMalloc(&gpu_gTemp, n*n*sizeof(int));
+
+	// Temporary pointer used for swapping gpu_G and gpu_gTemp
+	int *gpu_swapPtr;
+
+	// Define grid and block dimensions
+	dim3 dimGrid(GRID_SIZE, GRID_SIZE);
+	dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE);
+
+	// Iterate the number of desired times
+	for(int i = 0; i < k; i++){
+
+		// Call cudaKernel for each iteration using pointers to cuda memory
+		cudaKernel<<<dimGrid, dimBlock>>>(n, gpu_w, gpu_G, gpu_gTemp);
+
+		// Synchronize threads before swapping pointers
+		cudaThreadSynchronize();
+
+		// Swap gpu_G and gpu_gTemp pointers for next iteration to avoid copying data on every iteration
+		gpu_swapPtr = gpu_G;
+		gpu_G = gpu_gTemp;
+		gpu_gTemp = gpu_swapPtr;
+	}
+
+	// Copy final data to CPU memory
+	// If k is odd, due to pointer swapping on the GPU, current data is pointed to by gpu_G
+	if(k % 2 == 1)
+		cudaMemcpy(G, gpu_G, n*n*sizeof(int), cudaMemcpyDeviceToHost);
+	// If k is even, current data is pointed to by gpu_gTemp
+	else
+		cudaMemcpy(G, gpu_gTemp, n*n*sizeof(int), cudaMemcpyDeviceToHost);
+
+	// Free memory
+	//cudaFree(gpu_G);
+	//cudaFree(gpu_gTemp);
+
+}
+
+int main(){
+
+	// Set dimensions and number of iterations
+	int n = 517;	int k = 1;
+
+	// Open binary file and write contents to an array
+    FILE *fptr = fopen("conf-init.bin","rb");
+    printf("Pointer created\n");
+    int *G = calloc(n*n, sizeof(int));
+    printf("G allocated\n");
+    if (fptr == NULL){
+        printf("Error! opening file");
+        // Program exits if the file pointer returns NULL.
+        exit(1);
+    }
+
+    fread(G, sizeof(int), n*n, fptr);
+
+    // Define weights array
+    double weights[] = {0.004, 0.016, 0.026, 0.016, 0.004,
+    		0.016, 0.071, 0.117, 0.071, 0.016,
+			0.026, 0.117, 0, 0.117, 0.026,
+			0.016, 0.071, 0.117, 0.071, 0.016,
+			0.004, 0.016, 0.026, 0.016, 0.004};
+
+    // Call ising
+    ising(G, weights, k, n);
+
+    // Close binary file
+    fclose(fptr);
+    printf("Done");
+
+    return 0;
+}
+
