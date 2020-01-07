609f0b97d81d954a734759e88bd2425ca54e55da
CedArctic <kdoumani98@gmail.com>
2020-01-07 14:43:46 +0200

    Formating fixes to V3

--- cuda_multiple_moments_threads_shared_memory.c
+++ cuda_multiple_moments_threads_shared_memory.c
@@ -62,8 +62,9 @@ __global__ void cudaKernel(int n, int grid_size, double* gpu_w, int* gpu_G, int*
     // 1st caching method:     for(int sh_index = threadIdx.x; sh_index < CACHE_SIZE; sh_index += BLOCK_SIZE){
     // 2nd method: for(int sh_index = threadIdx.x  (CACHE_SIZE/BLOCK_THREADS + 1); (sh_index < (threadIdx.x + 1)  (CACHE_SIZE/BLOCK_THREADS + 1) + CACHE_SIZE/BLOCK_THREADS + 1) && (sh_index < CACHE_SIZE); sh_index ++){
     for(int sh_index = threadIdx.x * (CACHE_SIZE/BLOCK_THREADS + 1);
-    (sh_index < (threadIdx.x + 1) * (CACHE_SIZE/BLOCK_THREADS + 1) + CACHE_SIZE/BLOCK_THREADS + 1) && (sh_index < CACHE_SIZE);
-    sh_index ++){
+        (sh_index < (threadIdx.x + 1) * (CACHE_SIZE/BLOCK_THREADS + 1) + CACHE_SIZE/BLOCK_THREADS + 1) && (sh_index < CACHE_SIZE);
+        sh_index ++){
+
         // X and Y coordinates on the shared memory
         sh_x = sh_index % CACHE_LINE;
         sh_y = sh_index / CACHE_LINE;
