a0afda22e2c451245d84743e8f5bca662dc0b5ca
Unknown <kdoumani98@gmail.com>
2020-01-11 13:00:58 +0200

    Added early exit to V1

--- cuda_single_moment_threads.c
+++ cuda_single_moment_threads.c
@@ -71,6 +71,30 @@ __global__ void cudaKernel(int n, double* gpu_w, int* gpu_G, int* gpu_gTemp){
 	}
 }
 
+// Cuda kernel function used to check for early exit if G == gTemp
+__global__ void exitKernel(int n, int* gpu_G, int* gpu_gTemp, int* gpu_exitFlag){
+	
+	// Shared block exit flag
+    __shared__ int blockFlag = 0;
+	
+	// Calculate thread id
+	int thread_id = blockIdx.x * BLOCK_SIZE * BLOCK_SIZE + threadIdx.x;
+	
+	// If two values are not the same, increment the flag
+	// This is not race-condition safe but we don't care since one write is guaranteed to finish
+	if(gpu_gTemp[thread_id] == gpu_G[thread_id])
+		atomicAdd(&blockFlag, 1);
+	
+	// Sync threads before writing to global
+	__syncthreads();
+	
+	// First thread of the block writes flag back to the global memory
+	if((thread_id == blockIdx.x * BLOCK_SIZE * BLOCK_SIZE) && (blockFlag > 0))
+		atomicAdd(gpu_exitFlag, blockFlag);
+	
+}
+
+
 void printResult(int *G, int n){
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
@@ -103,6 +127,12 @@ void ising( int *G, double *w, int k, int n){
 
 	// Temporary pointer used for swapping gpu_G and gpu_gTemp
 	int *gpu_swapPtr;
+	
+	// GPU early exit flag
+	int *gpu_exitFlag;
+	int exitFlag = 0;
+	cudaMalloc(&gpu_exitFlag, sizeof(int));
+	cudaMemcpy(gpu_exitFlag, &exitFlag, sizeof(int), cudaMemcpyHostToDevice);
 
 	// Define grid and block dimensions - disabled for now
 	//dim3 dimGrid(GRID_SIZE, GRID_SIZE);
@@ -121,6 +151,14 @@ void ising( int *G, double *w, int k, int n){
 		gpu_swapPtr = gpu_G;
 		gpu_G = gpu_gTemp;
 		gpu_gTemp = gpu_swapPtr;
+		
+		// Check for early exit
+		exitKernel<<<grid_size * grid_size, BLOCK_SIZE*BLOCK_SIZE>>>(n, gpu_G, gpu_gTemp, gpu_exitFlag);
+		cudaDeviceSynchronize();
+		cudaMemcpy(&exitFlag, gpu_exitFlag, sizeof(int), cudaMemcpyDeviceToHost);
+		if(exitFlag > 0)
+			break;
+		
 	}
 
 	// Copy final data to CPU memory
